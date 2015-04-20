9ad458fd70703adaa02bdc58553c4908542fd43a
Ola Blissing <ola.blissing@gmail.com>
2015-04-20 10:15:17 +0200

    bootstrap modlab

---
+++ styles/Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      styles: {
+        files: ["custom/*.less"],
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    },
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yucompress: true,
+          optimization: 2
+        },
+        files: {
+          "build/custom.css": "custom/custom.less"
+        }
+      }
+    }
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['less', 'watch']);
+
+};
